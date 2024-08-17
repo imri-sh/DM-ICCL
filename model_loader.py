3554712f88b9d23e0c50df701b9ea3bb6e2594c2
imri shuval <80890006+imri-sh@users.noreply.github.com>
2024-08-17 18:43:41 +0300

    fixed model_name for flan-T5s

--- model_loader.py
+++ model_loader.py
@@ -2,6 +2,7 @@ from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, AutoModelForCausa
 
 
 def get_phi2():
+    """ google's phi-2. 2.7B params."""
     model_name = "microsoft/phi-2"
     model = AutoModelForCausalLM.from_pretrained(model_name)
     tokenizer = AutoTokenizer.from_pretrained(model_name, clean_up_tokenization_spaces=True)
@@ -9,6 +10,7 @@ def get_phi2():
 
 
 def get_phi3():
+    """ google's phi-3. 3.8B params."""
     model_name = "microsoft/Phi-3-mini-4k-instruct"
     model = AutoModelForCausalLM.from_pretrained(model_name)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -16,6 +18,7 @@ def get_phi3():
 
 
 def get_flan_T5_base():
+    """ Microsoft's FLAN T5 base. 250M params."""
     model_name = "google/flan-t5-base"
     model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
     tokenizer = AutoTokenizer.from_pretrained(model_name, clean_up_tokenization_spaces=True)
@@ -23,6 +26,7 @@ def get_flan_T5_base():
 
 
 def get_flan_T5_large():
+    """ Microsoft's FLAN T5 large. 780M params."""
     model_name = "google/flan-t5-large"
     model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
     tokenizer = AutoTokenizer.from_pretrained(model_name, clean_up_tokenization_spaces=True)
@@ -30,6 +34,7 @@ def get_flan_T5_large():
 
 
 def get_flan_T5_xl():
+    """ Microsoft's FLAN T5 xl. 3B params."""
     model_name = "google/flan-t5-xl"
     model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
     tokenizer = AutoTokenizer.from_pretrained(model_name, clean_up_tokenization_spaces=True)
