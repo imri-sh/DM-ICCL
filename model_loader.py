fa24b528e3d275d0f149ff21ed5d7b8afb8628a0
imri shuval <80890006+imri-sh@users.noreply.github.com>
2024-08-17 17:54:18 +0300

    fixed model_name for flan-T5s

--- model_loader.py
+++ model_loader.py
@@ -8,22 +8,29 @@ def get_phi2():
     return model, tokenizer
 
 
+def get_phi3():
+    model_name = "microsoft/Phi-3-mini-4k-instruct"
+    model = AutoModelForCausalLM.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    return model, tokenizer
+
+
 def get_flan_T5_base():
-    model_name = "microsoft/flan-t5-base"
+    model_name = "google/flan-t5-base"
     model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
     tokenizer = AutoTokenizer.from_pretrained(model_name, clean_up_tokenization_spaces=True)
     return model, tokenizer
 
 
 def get_flan_T5_large():
-    model_name = "microsoft/flan-t5-large"
+    model_name = "google/flan-t5-large"
     model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
     tokenizer = AutoTokenizer.from_pretrained(model_name, clean_up_tokenization_spaces=True)
     return model, tokenizer
 
 
 def get_flan_T5_xl():
-    model_name = "microsoft/flan-t5-xl"
+    model_name = "google/flan-t5-xl"
     model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
     tokenizer = AutoTokenizer.from_pretrained(model_name, clean_up_tokenization_spaces=True)
     return model, tokenizer
